de0f7fe1004f24650798a7b544a4a50fbe06b2ce
IllalRajinCoding <roxyuciha@gmail.com>
2025-05-13 21:40:00 +0700

    add auth

--- app/admin/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link"
-export default function Admin() {
-    return (
-        <div>
-            <h1>This admin</h1>
-            <Link href="/about" className="text-2xl border-4 ">To about</Link>
-        </div> 
-    )
-}
